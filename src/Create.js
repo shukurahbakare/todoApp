ea92e32635536a5de7bfbfe1fdf5d1eb0061e9a1
Shukura <bakaresukurah@outlook.com>
2023-07-31 08:02:22 -0700

    making a post request

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,53 @@
+import { useState } from "react";
+
 const Create = () => {
-    return (
-        <div className="create">
-            Create a new Blog
-        </div>
-      );
+  const [todo, setTitle] = useState('');
+  const [completed, setBody] = useState('');
+  const [userId, setUserId] = useState('');
+  const [isPending,setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {todo ,completed, userId};
+    setIsPending(true)
+   
+    fetch('https://dummyjson.com/todos/add',{
+        method:'POST',
+        headers:{ 'Content-Type':'application/json'},
+        body:JSON.stringify(data)
+    })
+    .then(setIsPending(false))
+    .then()
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Todo :</label>
+        <input 
+          type="text" 
+          required 
+          value={todo}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Status:</label>
+        <textarea
+          required
+          value={completed}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>User Id:</label>
+        <input
+          required
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        ></input>
+        { !isPending && <button>Add Todo</button>}
+        { isPending && <button disabled>Adding Todo</button>}
+      </form>
+    </div>
+  );
 }
  
 export default Create;
