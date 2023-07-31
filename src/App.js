f5cebac36c35dd0da053e559bc5f8096437bbcb3
Shukura <bakaresukurah@outlook.com>
2023-07-30 22:10:39 -0700

    react links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Create from './Create';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Switch>
           <Route path='/'>
             <Home/>
+          </Route>
+          <Route path='create'>
+            <Create />
           </Route>
         </Switch>
       </div>
---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return (
+        <div className="create">
+            Create a new Blog
+        </div>
+      );
+}
+ 
+export default Create;
