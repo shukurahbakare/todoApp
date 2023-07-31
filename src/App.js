4d39e501d9b8cc9a644df0f681d9c3a0ebf9ecfb
Shukura <bakaresukurah@outlook.com>
2023-07-31 07:02:24 -0700

    route prameters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <div className="content">
         <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='create' element={<Create />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/blogs/:id' element={<BlogDetails />} />
         </Routes>
       </div>
       </div>
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,12 +1,11 @@
 import {useState,useEffect} from 'react';
 
-
 const useFetch = (url) => {
-    const [data,setData] = useState ([null]);
-const [isPending, setIsPending] = useState(true);
-const [error,setError] = useState(null)
-    useEffect(() => {
+    const [data,setData] = useState (null);
+    const [isPending, setIsPending] = useState(true);
+    const [error,setError] = useState(null)
 
+    useEffect(() => {
         const abortCont = new AbortController();
 
         fetch(url, {signal: abortCont.signal})
